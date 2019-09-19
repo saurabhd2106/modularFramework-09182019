a22992d5b97b826da3a9eb53de80379d64809c18
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2019-09-20 00:35:57 +0530

    Window Handles control code

--- modularFramework-09182019/src/main/java/comonLibs/implementation/WindowControl.java
+++ modularFramework-09182019/src/main/java/comonLibs/implementation/WindowControl.java
@@ -2,32 +2,42 @@ package comonLibs.implementation;
 
 import java.util.Set;
 
+import org.openqa.selenium.WebDriver;
+
 import comonLibs.contracts.IWindow;
 
-public class WindowControl implements IWindow{
+public class WindowControl implements IWindow {
+	private WebDriver driver;
+
+	public WindowControl(WebDriver driver) {
+		this.driver = driver;
+	}
 
 	@Override
 	public void switchToAnyWindow(String windowhandle) throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		driver.switchTo().window(windowhandle);
 	}
 
 	@Override
 	public void switchToAnyWindow(int childWindowIndex) throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		String childWindow = driver.getWindowHandles().toArray()[childWindowIndex].toString();
+
+		driver.switchTo().window(childWindow);
+
 	}
 
 	@Override
 	public String getWindowHandle() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		return driver.getWindowHandle();
 	}
 
 	@Override
 	public Set<String> getWindowHandles() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		return driver.getWindowHandles();
 	}
 
 }
