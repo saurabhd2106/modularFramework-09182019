87e59555df0e93beb8acace91fc986a9b24c0702
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2019-09-19 23:38:53 +0530

    Alert control

--- modularFramework-09182019/src/main/java/comonLibs/implementation/AlertControl.java
+++ modularFramework-09182019/src/main/java/comonLibs/implementation/AlertControl.java
@@ -1,30 +1,38 @@
 package comonLibs.implementation;
 
+import org.openqa.selenium.WebDriver;
+
 import comonLibs.contracts.IAlert;
 
-public class AlertControl implements IAlert{
+public class AlertControl implements IAlert {
+	private WebDriver driver;
+
+	public AlertControl(WebDriver driver) {
+		this.driver = driver;
+	}
 
 	@Override
 	public void acceptAlert() throws Exception {
-		
-		
+
+		driver.switchTo().alert().accept();
+
 	}
 
 	@Override
 	public void rejectAlert() throws Exception {
-		
-		
+		driver.switchTo().alert().dismiss();
+
 	}
 
 	@Override
 	public String getMessageFromAlert() throws Exception {
-		
-		return null;
+
+		return driver.switchTo().alert().getText();
 	}
 
 	@Override
 	public boolean checkAlertPresent(int timeoutInseconds) throws Exception {
-		
+
 		return false;
 	}
 
