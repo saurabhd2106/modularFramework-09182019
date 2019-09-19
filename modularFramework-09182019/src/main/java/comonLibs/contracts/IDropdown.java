601401ca97c47ff001550f58a0bcf97cafb54912
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2019-09-19 22:37:47 +0530

    Dropdown control

--- modularFramework-09182019/src/main/java/comonLibs/contracts/IDropdown.java
+++ modularFramework-09182019/src/main/java/comonLibs/contracts/IDropdown.java
@@ -1,5 +1,14 @@
 package comonLibs.contracts;
 
+import org.openqa.selenium.WebElement;
+
 public interface IDropdown {
+	
+
+	public void selectViaVisibleText(WebElement element, String visibleText) throws Exception;
+
+	public void selectViaValue(WebElement element, String value) throws Exception;
+
+	public void selectViaIndex(WebElement element, int index) throws Exception;
 
 }
