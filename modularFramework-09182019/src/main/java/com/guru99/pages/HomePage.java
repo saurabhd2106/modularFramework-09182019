da5e4ba52d9133c1ca0e1075fe22a46a7a1763ef
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2019-10-03 22:53:00 +0530

    Added Excel Driver

--- modularFramework-09182019/src/main/java/com/guru99/pages/HomePage.java
+++ modularFramework-09182019/src/main/java/com/guru99/pages/HomePage.java
@@ -1,5 +1,23 @@
 package com.guru99.pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class HomePage extends BasePage {
+	private WebDriver driver;
+
+	private WebElement pageLink;
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+	}
+
+	public void navigateToPage(String pagename) throws Exception{
+
+		pageLink = driver.findElement(By.linkText(pagename));
+		elementControl.clickElement(pageLink);
+
+	}
 }
